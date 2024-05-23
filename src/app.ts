1acdca8cab3a8e757461f453acc57bfcb65cb011
Gunjan Datta <gudatta@microsoft.com>
2024-05-23 17:13:35 -0400

    Updated to use tiles

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import { Dashboard } from "dattatable";
-import { Components } from "gd-sprest-bs";
-import * as jQuery from "jquery";
+import { Components, CustomIcons, CustomIconTypes } from "gd-sprest-bs";
 import { DataSource, IListItem } from "./ds";
 import Strings from "./strings";
 
@@ -43,55 +42,102 @@ export class App {
                     }
                 ]
             },
-            table: {
-                rows: DataSource.List.Items,
-                dtProps: {
-                    dom: 'rt<"row"<"col-sm-4"l><"col-sm-4"i><"col-sm-4"p>>',
-                    createdRow: function (row, data, index) {
-                        jQuery('td', row).addClass('align-middle');
-                    },
-                    drawCallback: function (settings) {
-                        let api = new jQuery.fn.dataTable.Api(settings) as any;
-                        jQuery(api.context[0].nTable).removeClass('no-footer');
-                        jQuery(api.context[0].nTable).addClass('tbl-footer');
-                        jQuery(api.context[0].nTable).addClass('table-striped');
-                        jQuery(api.context[0].nTableWrapper).find('.dataTables_info').addClass('text-center');
-                        jQuery(api.context[0].nTableWrapper).find('.dataTables_length').addClass('pt-2');
-                        jQuery(api.context[0].nTableWrapper).find('.dataTables_paginate').addClass('pt-03');
-                    },
-                    headerCallback: function (thead, data, start, end, display) {
-                        jQuery('th', thead).addClass('align-middle');
-                    },
-                    // Order by the 1st column by default; ascending
-                    order: [[1, "asc"]]
+            tiles: {
+                items: DataSource.List.Items,
+                colSize: 3,
+                showFooter: false,
+                titleFields: ["MessageId"],
+                subTitleFields: ["Title"],
+                bodyFields: ["Summary"],
+                onHeaderRendered: (el, item: IListItem) => {
+                    el.classList.add("text-center");
+
+                    // Render the icons
+                    this.renderIcons(el, item);
                 },
-                columns: [
-                    {
-                        name: "MessageId",
-                        title: "Message Id"
-                    },
-                    {
-                        name: "Title",
-                        title: "Title"
-                    },
-                    {
-                        name: "Category",
-                        title: "Category"
-                    },
-                    {
-                        name: "Platform",
-                        title: "Platform"
-                    },
-                    {
-                        name: "Services",
-                        title: "Services",
-                        onRenderCell: (el, col, item: IListItem) => {
-                            // Combine the choice values
-                            el.innerHTML = item.Services.results.join(', ');
-                        }
-                    }
-                ]
             }
         });
+    }
+
+    // Renders the icons for the item
+    private renderIcons(el: HTMLElement, item: IListItem) {
+        // Define the height/width of the icons
+        let height = 32;
+        let width = 32;
+
+        // Parse the services
+        let elIconTypes = [];
+        for (let i = 0; i < item.Services.results.length; i++) {
+            let elIconType = null;
+            let service = item.Services.results[i];
+
+            switch (service) {
+                case "Azure Information Protection":
+                    elIconType = CustomIconTypes.aIP;
+                    break;
+
+                case "Exchange Online":
+                    elIconType = CustomIconTypes.exchange;
+                    break;
+
+                case "Microsoft 365 apps":
+                case "Microsoft 365 for the web":
+                case "Microsoft 365 suite":
+                    elIconType = CustomIconTypes.m365;
+                    break;
+
+                case "Microsoft Entra":
+                    elIconType = CustomIconTypes.entra;
+                    break;
+
+                case "Microsoft Forms":
+                    elIconType = CustomIconTypes.forms;
+                    break;
+
+                case "Microsoft Stream":
+                    elIconType = CustomIconTypes.stream;
+                    break;
+
+                case "Microsoft Teams":
+                    elIconType = CustomIconTypes.teams;
+                    break;
+
+                case "Microsoft Viva":
+                    elIconType = CustomIconTypes.viva;
+                    break;
+
+                case "SharePoint Online":
+                    elIconType = CustomIconTypes.sharePoint;
+                    break;
+
+                // Default Icon
+                default:
+                    break;
+            }
+
+            // See if this icon hasn't been rendered
+            if (elIconType && elIconTypes.indexOf(elIconType) < 0) {
+                // Create the icon
+                let elIcon = CustomIcons(elIconType, height, width);
+                elIcon.style.height = height.toString();
+                elIcon.style.width = width.toString();
+                elIcon.setAttribute("aria-hidden", "true");
+                elIcon.style.pointerEvents = "none";
+                elIcon.setAttribute("focusable", "false");
+
+                // Append the type
+                elIconTypes.push(elIconType);
+
+                // Add a tooltip
+                Components.Tooltip({
+                    content: service,
+                    type: Components.TooltipTypes.LightBorder,
+                    target: elIcon as any
+                });
+
+                // Append the icon
+                el.appendChild(elIcon);
+            }
+        }
     }
 }
